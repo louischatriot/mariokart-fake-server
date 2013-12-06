fa2fe9bc88ee81adb405ce7cd2c7f8a0763be258
Louis Chatriot <louis.chatriot@gmail.com>
2013-12-06 14:16:26 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,94 @@
+var express = require('express')
+  , app = express()
+  , config = require('./lib/config')
+  , cars = require('.lib/cars')
+  ;
+  
+app.use(express.bodyParser());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  return next();
+});
+app.use(app.router); // Map routes
+
+
+app.get('/su/mariokart/api/leaderboard', function (req, res, next) {
+  var leaderboard = []
+  
+  leaderboard.push({ user: { id: 1111, name: "Louis" }, score: 72 });
+  leaderboard.push({ user: { id: 1234, name: "Yohan" }, score: 156 });
+  leaderboard.push({ user: { id: 99, name: "Mike" }, score: 45 });
+
+  return res.json(leaderboard);
+});
+
+
+app.get('/su/mariokart/api/lastEvents', function (req, res, next) {
+  var events = []
+  
+  events.push({ type: "BANANA_PEEL_DROPPED"
+              , timestamp: new Date(2013, 11, 6, 10, 25, 36)
+              , byUser: { id: 1234, name: "Yohan" }
+              , additionalData: {}
+              });
+
+  events.push({ type: "BANANA_PEEL_DROPPED"
+              , timestamp: new Date(2013, 11, 6, 10, 52, 10)
+              , byUser: { id: 1234, name: "Yohan" }
+              , additionalData: {}
+              });
+
+  events.push({ type: "SLIPPED_ON_BANANA_PEEL"
+              , timestamp: new Date(2013, 11, 6, 11, 8, 54)
+              , byUser: { id: 1111, name: "Louis" }
+              , additionalData: { droppedBy: { id: 1234, name: "Yohan" } }
+              });              
+
+  return res.json(events);
+});
+
+
+app.get('/su/mariokart/api/bananaPeelsPositions', function (req, res, next) {
+  var positions = [];
+  
+  positions.push({ lat: 37.567516
+                 , lon: -122.324601
+                 , droppedBy: { id: 1234, name: 'Yohan' }
+                 , age: 23
+                 });
+                 
+  positions.push({ lat: 37.568749
+                 , lon: -122.326746
+                 , droppedBy: { id: 1234, name: 'Yohan' }
+                 , age: 5
+                 });
+
+  positions.push({ lat: 37.566717
+                 , lon: -122.32798
+                 , droppedBy: { id: 1111, name: 'Louis' }
+                 , age: 12
+                 });
+                
+  return res.json(positions);
+});
+
+
+app.get('/su/mariokart/api/carsPositions', function (req, res, next) {
+  var positions = [];
+  
+  positions.push({ lat: 37.571726
+                 , lon: -122.329913
+                 , driver: { id: 99, name: 'Mike' }
+                 });
+                 
+  positions.push({ lat: 37.568647
+                 , lon: -122.31985
+                 , driver: { id: 998, name: 'Clement' }
+                 });                
+                
+  return res.json(positions);
+});
+
+
+
+app.listen(config.svPort);
